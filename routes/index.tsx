4729a4edeee183f1579ce4f2392a6343abe2051b
P. Hughes <github@phugh.es>
2025-04-27 15:33:59 +0100

    Update iOS availability text with a link to the App Store

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         />
         <h1 class="text-4xl font-bold">HolyScroller.app</h1>
         <p class="mb-4 bold">Scroll the Scriptures!</p>
-        <p>Available on iOS and Android</p>
+        <p><a href="https://apps.apple.com/gb/app/holyscroller/id6744199137" title="App Store" target="_blank" rel="noopener noreferrer">Available on iOS for free</a></p>
         <p><a class="text-blue-500 underline" href="mailto:support@holyscroller.app">support@holyscroller.app</a></p>
         <p><a class="text-blue-500 underline" href="/privacy">Privacy Policy</a></p>
         <p class="mt-4 text-sm">Copyright 2025. All rights reserved.</p>
